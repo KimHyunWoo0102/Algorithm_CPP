2d1e35b4ca5afe3e778864e1514e676a768bdb9c
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-08-21 10:34:32 +0900

    week 1 : 11655 ROT13

---
+++ week1/11655.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string str,ret;
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	
+	getline(cin,str);
+	
+	for(char ch:str){
+		if('a'<=ch&&ch<='z'){
+			ret+= (ch-'a'+13)%26+'a';	
+		}else if('A'<=ch&&ch<='Z'){
+			ret+=(ch-'A'+13)%26+'A';
+		}else
+			ret+=ch;
+	}
+	
+	str=ret;
+	
+	for(char ch:str){
+		if('a'<=ch&&ch<='z'){
+			ret+= (ch-'a'+13)%26+'a';	
+		}else if('A'<=ch&&ch<='Z'){
+			ret+=(ch-'A'+13)%26+'A';
+		}else
+			ret+=ch;
+	}
+	
+	cout<<ret;
+	return 0;
+}
