c2d8250d5770f16143984c1715bc153b63e5c7b3
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-08-22 10:54:28 +0900

    week 1 : 1940
    combination

---
+++ week1/1940.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[15004];
+int cnt;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	
+	int n,m;
+	
+	cin>>n>>m;
+	
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+		
+	for(int i=0;i<n;i++){
+		for(int j=i;j<n;j++){
+			if(arr[i]+arr[j]==m){
+				cnt++;
+			}			
+		}
+	}
+	
+	cout<<cnt;
+	return 0;
+}
