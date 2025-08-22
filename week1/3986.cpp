bf19ffa199b956dfc23a8083b8db4454ff481028
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-08-22 10:55:17 +0900

    week 1 : 3986
    use stack when pairing examples

---
+++ week1/3986.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char prev;
+int cnt;
+
+int main(){
+	//ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	
+	string str;
+	int n;
+	
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+		stack<char> s;
+		cin>>str;
+		
+		for(char ch:str){
+			if(!s.empty()&&s.top()==ch){
+				s.pop();
+			}else{
+				s.push(ch);
+			}
+		}
+		
+		if(s.empty()){
+			cnt++;
+		}
+	}
+	
+	cout<<cnt;
+	return 0;
+}
