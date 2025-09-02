405c377fd4eb0230124790cd28179ca4fc14a4bf
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-09-02 11:30:18 +0900

    week 2 : 1068 트리 복습
    지역변수 명 조심

--- week2/1068.cpp
+++ week2/1068.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int cnt,n;
 vector<int> adj[56];
 
+int root;
+
 void go(int root){
-	
 	int child=0;
+	cout<<root<<"\n";
 	for(auto it:adj[root]){
 		if(it==n)continue;
 		go(it);
@@ -14,31 +16,36 @@ void go(int root){
 	}
 	
 	if(child==0){
+		cout<<root<<" has no child\n";
 		cnt++;
-		return;
+		return ;
 	}
 }
 int main(){
 	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 	
 	cin>>n;
-	
-	int root=0;
-	
+	int parent;
 	for(int i=0;i<n;i++){
-		int parent;
 		cin>>parent;
-		if(parent==-1)root=i;
-		else{
+		
+		if(parent==-1){
+			root=i;
+		}else {
 			adj[parent].push_back(i);
 		}
 	}
 	
+	for(int i=0;i<n;i++){
+		cout<<"parent : "<<i<<" child : ";
+		for(auto child:adj[i])
+			cout<<child<<" ";
+		cout<<'\n';
+	}
 	cin>>n;
 	if(n!=root){
 		go(root);
 	}
 	cout<<cnt;
-
 	return 0;
 }
