ad5923d04ad36e1f6f947c2c20b30882c894023f
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-09-04 16:41:18 +0900

    week 3 : 12581
    겹치는 경로에 대해서 고려

---
+++ week3/12581.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX_LEN 100004
+int n,k;
+int visited[MAX_LEN];
+int cnt=1;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin>>n>>k;
+	visited[n]=1;
+	
+	queue<int> q;
+	q.push(n);
+	
+	while(q.size()){
+		int pos=q.front();q.pop();
+		
+		for(int npos:{pos-1,pos+1,pos*2}){
+			if(npos<0||npos>=MAX_LEN)continue;
+			
+			if(!visited[npos]){
+				visited[npos]=visited[pos]+1;
+				q.push(npos);
+			}else if(visited[npos]==visited[pos]+1){
+				q.push(npos);
+				if(npos==k){
+					cnt++;
+				}
+			}
+		}
+	}	
+	
+	
+	cout<<visited[k]-1<<'\n'<<cnt;
+	return 0;
+}
